51255efcab826b9ddf8a8482494315cb4b527f01
Sagar Kumar <sagar.kumar@harness.io>
2024-08-26 21:19:07 +0530

    feat: update docs
    Signed-off-by: Sagar Kumar <sagar.kumar@harness.io>

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -278,76 +278,6 @@ const docTemplate = `{
                 }
             }
         },
-        "/incident/get": {
-            "get": {
-                "description": "Get incident",
-                "consumes": [
-                    "application/json"
-                ],
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "Incident Management"
-                ],
-                "summary": "Get incident",
-                "operationId": "GetIncident",
-                "parameters": [
-                    {
-                        "type": "string",
-                        "description": "incident identifier",
-                        "name": "incidentIdentifier",
-                        "in": "path",
-                        "required": true
-                    },
-                    {
-                        "type": "string",
-                        "description": "accountIdentifier",
-                        "name": "accountIdentifier",
-                        "in": "query",
-                        "required": true
-                    },
-                    {
-                        "type": "string",
-                        "description": "orgIdentifier",
-                        "name": "orgIdentifier",
-                        "in": "query"
-                    },
-                    {
-                        "type": "string",
-                        "description": "projectIdentifier",
-                        "name": "projectIdentifier",
-                        "in": "query"
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/incident.GetResponseDTO"
-                        }
-                    },
-                    "400": {
-                        "description": "Bad Request",
-                        "schema": {
-                            "$ref": "#/definitions/utils.DefaultResponseDTO"
-                        }
-                    },
-                    "404": {
-                        "description": "Not Found",
-                        "schema": {
-                            "$ref": "#/definitions/utils.DefaultResponseDTO"
-                        }
-                    },
-                    "500": {
-                        "description": "Internal Server Error",
-                        "schema": {
-                            "$ref": "#/definitions/utils.DefaultResponseDTO"
-                        }
-                    }
-                }
-            }
-        },
         "/incident/list": {
             "get": {
                 "description": "List incidents",
@@ -470,6 +400,76 @@ const docTemplate = `{
                         "description": "OK",
                         "schema": {
                             "$ref": "#/definitions/incident.ListResponseDTO"
+                        }
+                    },
+                    "400": {
+                        "description": "Bad Request",
+                        "schema": {
+                            "$ref": "#/definitions/utils.DefaultResponseDTO"
+                        }
+                    },
+                    "404": {
+                        "description": "Not Found",
+                        "schema": {
+                            "$ref": "#/definitions/utils.DefaultResponseDTO"
+                        }
+                    },
+                    "500": {
+                        "description": "Internal Server Error",
+                        "schema": {
+                            "$ref": "#/definitions/utils.DefaultResponseDTO"
+                        }
+                    }
+                }
+            }
+        },
+        "/incident/{incidentIdentifier}": {
+            "get": {
+                "description": "Get incident",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "Incident Management"
+                ],
+                "summary": "Get incident",
+                "operationId": "GetIncident",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "incident identifier",
+                        "name": "incidentIdentifier",
+                        "in": "path",
+                        "required": true
+                    },
+                    {
+                        "type": "string",
+                        "description": "accountIdentifier",
+                        "name": "accountIdentifier",
+                        "in": "query",
+                        "required": true
+                    },
+                    {
+                        "type": "string",
+                        "description": "orgIdentifier",
+                        "name": "orgIdentifier",
+                        "in": "query"
+                    },
+                    {
+                        "type": "string",
+                        "description": "projectIdentifier",
+                        "name": "projectIdentifier",
+                        "in": "query"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/incident.GetResponseDTO"
                         }
                     },
                     "400": {
