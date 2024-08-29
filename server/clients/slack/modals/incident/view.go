bb52bf2b87ac39a3313069bc160e05fc103bfdd0
Raj Babu Das <mail.rajdas@gmail.com>
2024-08-29 18:29:28 +0530

    feat: Adding slack view and list handlers for incidents
    Signed-off-by: Raj Babu Das <mail.rajdas@gmail.com>

--- server/clients/slack/modals/incident/view.go
+++ server/clients/slack/modals/incident/view.go
@@ -114,7 +114,7 @@ func (is incidentService) ShowIncident(evt *socketmode.Event, incidentID string)
 
 	commander = formatUserMention(commander)
 	communicationsLead = formatUserMention(communicationsLead)
-	startedAt := time.Unix(getIncident.CreatedAt, 0).Format("Monday, Jan 2, 2006 at 3:04 PM")
+	startedAt := time.Unix(getIncident.CreatedAt, 0).UTC().Format("Monday, Jan 2, 2006 at 3:04 PM")
 
 	blocks := []slack.Block{
 		slack.NewSectionBlock(
@@ -160,7 +160,7 @@ func (is incidentService) ShowIncident(evt *socketmode.Event, incidentID string)
 	}
 
 	if getIncident.Status == incident.Resolved {
-		completedAt := time.Unix(*getIncident.UpdatedAt, 0).Format("Monday, Jan 2, 2006 at 3:04 PM")
+		completedAt := time.Unix(getIncident.UpdatedAt, 0).UTC().Format("Monday, Jan 2, 2006 at 3:04 PM")
 		blocks = append(blocks, slack.NewSectionBlock(
 			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(":checkered_flag: *Completed At:* %s", completedAt), false, false),
 			nil,
