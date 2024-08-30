4a24c9fc46a28838e2b2ced18257668e1c7c3f3b
Raj Das <mail.rajdas@gmail.com>
2024-08-30 13:23:12 +0530

    fix: updatedAt nil pointer check (#35)
    * fix: updatedAt nil pointer check
    
    Signed-off-by: Raj Babu Das <mail.rajdas@gmail.com>
    
    * Delete .run/server.run.xml
    
    ---------
    
    Signed-off-by: Raj Babu Das <mail.rajdas@gmail.com>

--- server/clients/slack/modals/incident/view.go
+++ server/clients/slack/modals/incident/view.go
@@ -159,7 +159,7 @@ func (is incidentService) ShowIncident(evt *socketmode.Event, incidentID string)
 		),
 	}
 
-	if getIncident.Status == incident.Resolved {
+	if getIncident.Status == incident.Resolved && getIncident.UpdatedAt != nil {
 		completedAt := time.Unix(*getIncident.UpdatedAt, 0).UTC().Format("Monday, Jan 2, 2006 at 3:04 PM")
 		blocks = append(blocks, slack.NewSectionBlock(
 			slack.NewTextBlockObject(slack.MarkdownType, fmt.Sprintf(":checkered_flag: *Completed At:* %s", completedAt), false, false),
